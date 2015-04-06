29e7e13ddc2e6d66347138ba74af5321596dc948
Kasper Honnens de Lichtenberg <kasper@honnens.dk>
2015-04-06 09:58:36 +0200

    changed gravity class to work for all children of the Sphere instead of just two

--- Assets/Scripts/GravityClass.cs
+++ Assets/Scripts/GravityClass.cs
@@ -3,14 +3,14 @@ using System.Collections;
 
 public class GravityClass : MonoBehaviour {
 
-	public Transform bodyPrefab; 
+	public Transform bodyPrefab;
 	public Transform body;
 
 	// Use this for initialization
 	void Start () {
-	
+
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		if(Input.GetKeyDown (KeyCode.A)){
@@ -22,21 +22,19 @@ public class GravityClass : MonoBehaviour {
 			Debug.Log(transform.childCount);
 		}
 
-		if(transform.childCount == 2){
 			updateChildren();
-		}
 	}
 
 	public void updateChildren(){
 
-		Vector3 force1 = gravitationalForceCalculator(transform.GetChild(0), transform.GetChild(1));
-		Vector3 force2 = gravitationalForceCalculator(transform.GetChild(1), transform.GetChild(0));
-
-		Vector3 accVector1 = accelerationVector(transform.GetChild(0), force1);
-		Vector3 accVector2 = accelerationVector(transform.GetChild(1), force2);
-
-		transform.GetChild(0).GetComponent<Rigidbody>().AddForce(accVector1);
-		transform.GetChild(1).GetComponent<Rigidbody>().AddForce(accVector2);
+		for(int i = 0;i<transform.childCount;i++){
+			for(int j = 0;j<transform.childCount;j++){
+				if(i==j)continue;
+				Vector3 force = gravitationalForceCalculator(transform.GetChild(i), transform.GetChild(j));
+				Vector3 acc = force/transform.GetChild(i).GetComponent<Rigidbody>().mass;
+				transform.GetChild(i).GetComponent<Rigidbody>().AddForce(acc);
+			}
+		}
 	}
 
 	public Vector3 gravitationalForceCalculator(Transform body1, Transform body2){
@@ -46,8 +44,8 @@ public class GravityClass : MonoBehaviour {
 		float g = 6;
 		Vector3 pos1 = body1.transform.position;
 		Vector3 pos2 = body2.transform.position;
-		float distance = Mathf.Sqrt(Mathf.Pow((pos1.x-pos2.x),2) + 
-									  Mathf.Pow((pos1.y-pos2.y),2) + 
+		float distance = Mathf.Sqrt(Mathf.Pow((pos1.x-pos2.x),2) +
+									  Mathf.Pow((pos1.y-pos2.y),2) +
 									  Mathf.Pow((pos1.z-pos2.z),2));
 		Vector3 unitVector = ((pos1-pos2)/distance);
 
@@ -68,7 +66,7 @@ public class GravityClass : MonoBehaviour {
 
 	public void objectInstantiater(float size, Vector3 pos, float mass){
 		body = Object.Instantiate(bodyPrefab, pos, Quaternion.identity) as Transform;
-		
+
 		Vector3 scale = new Vector3(size, size, size);
 		body.localScale = scale;
 		body.GetComponent<Rigidbody>().mass = mass;
