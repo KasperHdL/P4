c1b877f5aafd636dae7dcb473ee2be4dacda8f67
Kasper Honnens de Lichtenberg <kasper@honnens.dk>
2015-04-09 13:23:42 +0200

    camera work - not perfect yet

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -4,11 +4,66 @@ using System.Collections;
 public class CameraMovement : MonoBehaviour {
 
 	public Transform target;
-	public Vector3 offset;
+	public float offset;
+	public float scrollSpeed = 20f;
+
+	public bool mouseDrag = false;
+	public Vector3 startDragPos = Vector3.zero;
+
+	public float dragSpeed = 1f;
+
+	private Vector3 direction;
+
+	void Start(){
+		if(target == null)
+			Debug.LogError("Camera target is not set!");
+
+		direction = Vector3.forward;
+		transform.rotation = Quaternion.LookRotation(-direction);
+		transform.position = target.position + direction * offset;
+
+	}
 
 	void Update () {
-		if(target != null){
-			transform.position = target.position + offset;
+		if(target == null)
+			return;
+
+		mouseDragUpdate();
+	}
+
+	void mouseDragUpdate(){
+		Vector3 delta = transform.position - target.position;
+
+		offset -= Input.mouseScrollDelta.y * scrollSpeed;
+
+		if(mouseDrag){
+			//update
+			if(Input.GetMouseButtonUp(0))
+				mouseDrag = false;
+			else{
+				Vector3 mousePosition = new Vector3(-Input.mousePosition.x,Input.mousePosition.y,0);;
+				Vector3 to = startDragPos - mousePosition;
+				to *= dragSpeed;
+				to += delta;
+
+
+				direction = Vector3.RotateTowards(delta,to,Time.deltaTime,0f);
+
+				Debug.DrawRay(target.position, direction, Color.red);
+
+				transform.rotation = Quaternion.LookRotation(-direction);
+
+				startDragPos = mousePosition;
+			}
+
+		}else if(Input.GetMouseButtonDown(0)){
+			mouseDrag = true;
+			startDragPos = Input.mousePosition;
 		}
+
+
+		direction = direction.normalized * offset;
+		transform.position = target.position + direction;
+
 	}
 }
