83f9dce28a2356c29fc523e51ed4d938483d3fd2
Kasper Honnens de Lichtenberg <kasper@honnens.dk>
2015-05-11 11:30:45 +0200

    demoday test prototype

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,144 +1,121 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.IO;
 
 public class Spawner : MonoBehaviour {
 
-	public GameObject obj;
-	int counter = 0;
-	int correct = 0;
-	void Start(){
-		Instantiate(obj, RandomPosition(), Quaternion.identity);
+	public GameObject prefab;
+	private GameObject obj;
 
+	private readonly bool drawCube = false;
+
+
+	public Transform direction;
+
+	private bool roundStarted = false;
+
+	private bool waitForRestart = false;
+
+
+
+	private readonly string directory = "D:/KasperHdL/Desktop/TestFiles/";
+	private readonly string filenamePrepend = "participant_";
+	private readonly string filenameAppend = ".txt";
+	private int nextNum = 0;
+
+	private readonly int maxRounds = 10;
+
+	private int round = 0;
+	private float[] correctAngles = new float[10];
+	private float[] angles = new float[10];
+
+	private float correctAngle;
+
+	private float offset = 5;
+
+
+	void Start(){
+		findNextFileNumber();
+	}
+
+	void init(){
+		findNextFileNumber();
+		round = 0;
+
+		correctAngles = new float[10];
+		angles = new float[10];
+		roundStarted = false;
+		waitForRestart = false;
+	}
+
+	void findNextFileNumber(){
+		DirectoryInfo dir = new DirectoryInfo(directory);
+
+		int max = 0;
+
+		foreach (FileInfo file in dir.GetFiles()){
+			if(file.Name == filenamePrepend + max + filenameAppend)
+				max++;
+
+		}
+
+		nextNum = max;
 	}
 
 	// Update is called once per frame
 	void Update () {
+		if(roundStarted){
+			//update and wait for click
 
+	        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-		if(Input.GetMouseButtonDown(0)){
-			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-			RaycastHit hit;
-			if(Physics.Raycast(ray, out hit)){
-				//Debug.Log (hit.transform.name);
+	        RaycastHit hitinfo;
+	        if (Physics.Raycast(ray, out hitinfo, Mathf.Infinity, 1<<8)){
+				Vector3 mPos = hitinfo.point;
 
-				if(counter < 20){
-					if(hit.collider.gameObject.tag == "BackRight"){
-						Destroy(GameObject.FindWithTag("SoundCube"));
-						Instantiate(obj, RandomPosition(), Quaternion.identity);
-						counter += 1;
-						Debug.Log("BACK - WRONG");
-					}
-					if(hit.collider.gameObject.tag == "Front"){
-						Destroy(GameObject.FindWithTag("SoundCube"));
-						Instantiate(obj, RandomPosition(), Quaternion.identity);
-						counter += 1;
-						Debug.Log("FRONT - WRONG");
+				float angle = Vector3.Angle(Vector3.right,mPos);
+				direction.LookAt(mPos);
 
-					}
-					if(hit.collider.gameObject.tag == "Left"){
-						Destroy(GameObject.FindWithTag("SoundCube"));
-						Instantiate(obj, RandomPosition(), Quaternion.identity);
-							counter += 1;
+				 if(Input.GetMouseButtonDown(0)){
+		        	//clicked
+				 	correctAngles[round-1] = correctAngle;
 
-						Debug.Log("LEFT - WRONG");
-
-					}
-					if(hit.collider.gameObject.tag == "Right"){
-						Destroy(GameObject.FindWithTag("SoundCube"));
-						Instantiate(obj, RandomPosition(), Quaternion.identity);
-							counter += 1;
-
-						Debug.Log("RIGHT - WRONG");
-
-					}
-
-					if(hit.collider.gameObject.tag == "BackLeft"){
-						Destroy(GameObject.FindWithTag("SoundCube"));
-						Instantiate(obj, RandomPosition(), Quaternion.identity);
-							counter += 1;
-
-						Debug.Log("RIGHT - WRONG");
-						
-					}
-
-					if(hit.collider.gameObject.tag == "FrontLeft"){
-						Destroy(GameObject.FindWithTag("SoundCube"));
-						Instantiate(obj, RandomPosition(), Quaternion.identity);
-							counter += 1;
-
-						Debug.Log("RIGHT - WRONG");
-						
-					}
-
-					if(hit.collider.gameObject.tag == "FrontRight"){
-						Destroy(GameObject.FindWithTag("SoundCube"));
-						Instantiate(obj, RandomPosition(), Quaternion.identity);
-							counter += 1;
-
-						Debug.Log("RIGHT - WRONG");
-						
-					}
-					if(hit.collider.gameObject.tag == "SoundCube"){
-						Destroy(GameObject.FindWithTag("SoundCube"));
-						Instantiate(obj, RandomPosition(), Quaternion.identity);
-							counter += 1;
-						correct += 1;
-						Debug.Log ("CORRECT!!!!!!");
-
-					}
-
-
+				 	angles[round-1] = (angle/180)*Mathf.PI * (mPos.y>0 ? -1:1);
+				 	Destroy(obj);
+				 	roundStarted = false;
+		        }
+	        }
+		}else if(waitForRestart){
+			if(Input.GetKeyDown(KeyCode.Space)){
+				init();
+			}
+		}else{
+			if(round == maxRounds){
+				string s = "angle,\t\tcorrect angle;\r\n";
+				for(int i = 0;i<maxRounds;i++){
+					s += angles[i].ToString("F10") + ",\t" + correctAngles[i].ToString("F10") + ";\r\n";
 				}
+ 				File.WriteAllText(directory + filenamePrepend + nextNum + filenameAppend, s);
 
+ 				findNextFileNumber();
+ 				waitForRestart = true;
+			}else{
+				newRound();
+				roundStarted = true;
 			}
-
-			if(counter == 20){
-				Debug.Log("You got " + correct + " correct answers");
-				counter += 1;
-			}
-
 		}
+
+
 
 	}
 
-	Vector3 RandomPosition()
-	{
-		int rnd = Random.Range(1,8);
-		Vector3 pos = new Vector3(0, 0, 0);
-		switch (rnd)
-		{
-		case 1:
-			//Front
-			pos = new Vector3(0,25,300);
-			break;
-			
-		case 2:
-			pos = new Vector3(200,25,200);
-			break;
-			
-		case 3:
-			//Right
-			pos = new Vector3(300,25,0);
-			break;
-			
-		case 4:
-			//Left
-			pos = new Vector3(-300,25,0);
-			break;
+	private void newRound(){
+		float angle = Random.Range(-Mathf.PI,Mathf.PI);
 
-		case 5:
-			pos = new Vector3(-200,25,200);
-			break;
-		
-		case 6:
-			pos = new Vector3(200,25,-200);
-			break;
+		correctAngle = angle;
 
-		case 7:
-			pos = new Vector3(-200,25,-200);
-			break;
-		}
-		
-		return pos;
+		obj = Instantiate(prefab, new Vector3(Mathf.Cos(angle)*offset,(drawCube ? 0:-10),Mathf.Sin(angle)*offset), Quaternion.identity) as GameObject;
+		round++;
 	}
+
 }
