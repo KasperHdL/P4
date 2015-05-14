8498784f2a92398a2c3cddc0ea36e60c01c53639
Itai Yavin <Itai.yavin@gmail.com>
2015-05-14 10:29:34 +0200

    Added a buttons for all made bodies. Pressing a button will change camera target to the specific body.

--- Assets/Scripts/UI/ButtonWithText.cs
+++ Assets/Scripts/UI/ButtonWithText.cs
@@ -5,10 +5,12 @@ using UnityEngine.EventSystems;
 public class ButtonWithText : Button {
 	public Text text;
 	public TemperatureSlider temp;
+	public PlanetSwitcher planetSwitcher;
 	public int index;
 
 	public override void OnPointerClick(PointerEventData eventData){
 		if(temp != null)temp.handleJumpButton(index);
+		else if(planetSwitcher != null)planetSwitcher.handleButton(index);
 		base.OnPointerClick(eventData);
 	}
 }
---
+++ Assets/Scripts/UI/PlanetSwitcher.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class PlanetSwitcher : MonoBehaviour {
+	public 	Transform buttonPrefab;
+	public 	CameraMovement camMov;
+	public 	GravitySystem gs;
+
+	private List<Button> buttons;
+	private int currentBodyCount;
+	private float buttonWidth = 50;
+	private float panelWidth;
+	private float panelHeight = 60;
+
+	private RectTransform panelRT;
+	private RectTransform buttonRT;
+
+	private float buttonSpace = 5;
+	private float buttonOffset = 5;
+
+	// Use this for initialization
+	void Start () {
+
+		panelRT 			= transform as RectTransform;
+		panelWidth 			= panelRT.rect.width;
+		panelHeight 		= panelRT.rect.height;
+
+		currentBodyCount 	= 0;
+		buttons 			= new List<Button>();
+	}
+
+	public void newBody(Body body){
+		currentBodyCount++;
+		updateButtons();
+	}
+
+	public void handleButton(int i){
+		if(gs.bodies[i] != null){
+			camMov.setTarget(gs.bodies[i].transform);
+		} else
+			Debug.Log("No body found!");
+	}
+
+	public void updateButtons(){
+		RectTransform t = Instantiate(buttonPrefab, Vector3.zero, Quaternion.identity) as RectTransform;
+		ButtonWithText b = t.GetComponent<ButtonWithText>();
+		b.index = currentBodyCount-1;
+		b.planetSwitcher = this;
+
+		panelWidth = (buttonWidth+buttonSpace)*currentBodyCount + buttonSpace;
+		panelRT.sizeDelta = new Vector2(panelWidth, panelHeight);
+
+		t.SetParent(transform);
+		t.anchoredPosition = new Vector2((buttonWidth+buttonSpace)*currentBodyCount - buttonWidth/2,buttonOffset/2);
+
+
+	}
+}
