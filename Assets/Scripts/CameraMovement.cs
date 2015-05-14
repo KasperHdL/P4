8572b63a390efc84b91e8dceb341771fae85d256
Kasper Honnens de Lichtenberg <kasper@honnens.dk>
2015-05-14 15:16:33 +0200

    implemented camera zoom close #23

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -9,6 +9,7 @@ public class CameraMovement : MonoBehaviour {
 	public Transform target;
 	private Body body;
 	public float offset;
+	public float scrollSpeed = 100;
 
 	void Start(){
 		cam.transform.rotation = Quaternion.LookRotation(-Vector3.up);
@@ -19,11 +20,20 @@ public class CameraMovement : MonoBehaviour {
 	}
 
 	void Update () {
+		offset += -Input.mouseScrollDelta.y * scrollSpeed;
+
+
+		if(offset < 50 + (target == null ? 0 : body.radius))
+			offset = 50 + (target == null ? 0 : body.radius);
+		else if(offset > 90050)
+			offset = 90050;
+
+
 		if(target == null){
 			cam.transform.localPosition = Vector3.up * offset;
 		}else{
 			transform.position = body.positions[(gs.uiHold ? 0 : (int)(Time.timeScale*10))];
-			cam.transform.localPosition = Vector3.up * (offset + body.radius);
+			cam.transform.localPosition = Vector3.up * offset;
 		}
 	}
 
